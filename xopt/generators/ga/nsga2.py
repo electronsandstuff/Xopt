169da1ea7b231680ed415d71c527eadc707b1c52
Christopher M. Pierce <contact@chris-pierce.com>
2026-08-28 12:46:03 -0700

    remove extra imports

--- xopt/generators/ga/nsga2.py
+++ xopt/generators/ga/nsga2.py
@@ -1,7 +1,6 @@
 from itertools import chain
 from pydantic import Field, Discriminator, model_validator
 from typing import Annotated
-import logging
 import numpy as np
 import pandas as pd
 import time
@@ -11,8 +10,6 @@ from xopt.vocs import get_constraint_data, get_objective_data, get_variable_data
 from ...errors import DataError
 from ...generator import StateOwner
 from ...vocs import VOCS
-from ..checkpoints import CheckpointMixin
-from ..deduplicated import DeduplicatedGeneratorBase
 from ..utils import fast_dominated_argsort
 from .base import GAGeneratorBase
 from .operators import (
